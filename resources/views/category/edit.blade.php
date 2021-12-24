24eabddb7e34e8b3756de92036dce3685bae871c
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-12-24 10:36:48 -0300

    listo modelo de db y datos creando vistas

---
+++ resources/views/category/edit.blade.php
@@ -0,0 +1,8 @@
+<form action="" method="post">
+    @method{{PUT}}
+    <div class="form-group">
+         <label for="name" class="form-label">Name:</label>
+         <input type="text" class="form-control" name="name" id="name" placeholder="input name">
+     </div>
+     <button type="submit" class="btn btn-primary">Save changes</button>
+ </form>
