3839273f96c0cde5e0fdfa94691c7d975bcf6ec6
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-12-27 15:00:41 -0300

    se corrigio vista

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-12 text-center">
-<a href="{{ route('products.create') }}" class="btn btn-success">➕</a>
+            <div class="col-12 text-center m-3">
+<a href="{{ route('products.create') }}" class="btn btn-success rounded-circle"><i class="fas fa-plus"></i></a>
             </div>
             @forelse ($products as $products)
             <div class="card" style="width: 18rem;">
@@ -11,9 +11,9 @@
                   <h5 class="card-title">Name: {{$products->name}}</h5>
                   <p class="card-text">Price: {{$products->price}}</p>
                   <p class="card-text">Description: {{$products->description}}</p>
-                  <a href="{{route('products.show',$products->id)}}" class="btn btn-primary">Go</a>
+                  <a href="{{route('products.show',$products->id)}}" class="btn btn-primary form-control">View</a>
                   <div class="d-flex justify-content-center">
-                        <a href="{{route('products.edit',$products->id)}}" class="btn btn-secondary"></a>
+                        <a href="{{route('products.edit',$products->id)}}" class="btn btn-secondary"><i class="fas fa-edit"></i></a>
 
                         <form action="{{ route('products.destroy', $products->id) }}" method="POST">
                             @csrf
